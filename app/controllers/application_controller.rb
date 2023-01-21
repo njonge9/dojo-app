5394c5a7818e6933f6e60bd6077bc73e3654f519
Njonge9 <frednjonge9@gmail.com>
2023-01-22 00:41:43 +0300

    Autoloading and Reloading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+# You should  not load anything under app with require
+# This feature is called autoloading and reloading constants
+# You only need require calls for two purposes
+# 1.To load files under lib directory
+# 2.To load dependancies that have require: false in the Gemfile
 class ApplicationController < ActionController::Base
 end
