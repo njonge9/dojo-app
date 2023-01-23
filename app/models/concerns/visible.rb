0a2e3928e743e1f1c95a6bfdd28d5e9095a6ca26
Njonge9 <frednjonge9@gmail.com>
2023-01-23 12:21:25 +0300

    styling the app

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,3 +1,4 @@
+# store all the status methods that were duplicated in the models
 module Visible
   extend ActiveSupport::Concern
 
@@ -5,6 +6,13 @@ module Visible
 
   included do
     validates :status, inclusion: {in: VALID_STATUSES}
+  end
+
+  # display count of public articles or comments on our main page
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
   end
 
   def archived?
